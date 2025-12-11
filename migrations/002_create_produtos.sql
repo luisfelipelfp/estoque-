9a5be9b895f3a9bbc8f09ab2968995f1ee49bd4e
luisfelipelfp <luis.lfp.felipe@gmail.com>
2025-12-10 21:13:43 -0300

    Update 002_create_produtos.sql

--- migrations/002_create_produtos.sql
+++ migrations/002_create_produtos.sql
@@ -1,1 +1,15 @@
+-- Migration 002 - Create produtos table
+
+DROP TABLE IF EXISTS `produtos`;
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `quantidade` int(11) NOT NULL DEFAULT 0,
+  `ativo` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nome` (`nome`)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_uca1400_ai_ci;
 
